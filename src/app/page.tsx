febafda94a6ef7dee58bbe3528a03cbcd3fb7371
claire-d-b <claire.deberny@essca.eu>
2024-07-22 07:50:02 +0200

    label color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,50 @@
-import Cexample from "./Cexample"
-import Ctitle from "./Ctitle"
-import { TextField } from "@mui/material"
+// import Cexample from "./Cexample"
+// import Ctitle from "./Ctitle"
+// import { TextField } from "@mui/material"
+
+// export default function Home() {
+//   return (
+//     <div className="w-full">
+//       <Cexample opts="p-4 flex flex-col w-full h-full justify-center items-center bg-gray-200">
+//         <Ctitle opts="text-lg font-light text-white" title="Matcha" />
+//       </Cexample>
+//       <div className="p-2 flex flex-col h-full w-full justify-center items-center">
+//         <div className="gap-2 flex flex-col">
+//           <TextField InputProps={{ sx: { borderRadius: '0.75rem' } }} type="email" size="small" variant="outlined" label="email" />
+//           <TextField InputProps={{ sx: { borderRadius: '0.75rem' } }} type="password" size="small" variant="outlined" label="password" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// App.js
+import React from 'react';
+import { CssBaseline } from '@mui/material'
+import Ctextfield from './Ctextfield'
+import Ctitle from './Ctitle'
+import Image from 'next/image';
+import TeaImage from './assets/tea.png'
+
 
 export default function Home() {
   return (
-    <div className="w-full">
-      <Cexample opts="p-4 flex flex-col w-full h-full justify-center items-center bg-gray-200">
-        <Ctitle opts="text-lg font-light text-white" title="Matcha" />
-      </Cexample>
-      <div className="p-2 flex flex-col h-full w-full justify-center items-center">
-        <div className="gap-2 flex flex-col">
-          <TextField InputProps={{ sx: { borderRadius: '0.75rem' } }} type="email" size="small" variant="outlined" label="email" />
-          <TextField InputProps={{ sx: { borderRadius: '0.75rem' } }} type="password" size="small" variant="outlined" label="password" />
+    <div>
+      <CssBaseline />
+        <div className="w-full">
+        <div className="p-4 flex flex-col w-full h-full justify-center items-center bg-gray-200">
+          <Ctitle opts="text-lg font-light text-white" title="Matcha" />
+        </div>
+        <div className="p-2 flex flex-col h-full w-full justify-center items-center">
+          <div className="gap-2 flex flex-col">
+            <Ctextfield type="email" size="small" variant="outlined" label="email" />
+            <Ctextfield type="password" size="small" variant="outlined" label="password" />
+            <Image src={TeaImage} alt="matcha" />
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+
