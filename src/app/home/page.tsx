a7c9cacf388e85ec2b2c0fb772ebda8fa59c3c76
claire-d-b <claire.deberny@essca.eu>
2024-07-23 21:12:26 +0200

    change homepage design

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,28 +12,28 @@ import theme from '../theme'
 const _ = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className="flex flex-col h-screen w-screen bg-gray-300">
-        <Paper className="p-4 border border-white flex w-full justify-center items-center bg-gray-800">
+      <div className="flex flex-col h-screen w-screen justify-center items-stretch">
+        {/* <Paper className="p-4 border border-white flex w-full justify-center items-center bg-gray-800">
           <Ctitle opts="text-lg font-light" title="Matcha" />
           <Image className="w-10 h-auto" src={LeafImage} alt="leaf" />
           <Cnav className="self-end flex w-full justify-end items-center gap-12" />
-        </Paper>
-        <div className='w-full h-full flex items-center justify-center'>
-          <div className='w-1/4 flex-shrink'></div>
-          <Paper className="rounded-none h-full w-1/2 p-2 flex flex-col flex-grow flex-shrink-0 justify-center items-center">
-            <div className="pt-10 gap-2 flex flex-col justify-start items-center">
-              <Ctextfield opts="" type="email" size="small" variant="outlined" label="email" multiline={false} />
-              <Ctextfield opts="" type="password" size="small" variant="outlined" label="password" multiline={false} />
-              <Image className="w-48 h-auto" src={TeaImage} alt="matcha" />
+        </Paper> */}
+        {/* <div className='w-full h-full flex items-center justify-center'> */}
+          {/* <div className='w-1/4 flex-shrink'></div> */}
+          <Paper className="rounded-none h-full p-2 flex flex-col flex-grow flex-shrink-0 justify-center items-center text-lg font-thin">
+            <div className="w-full pt-10 gap-2 flex flex-col justify-center items-center">
+              <Image className="w-24 h-auto" src={LeafImage} alt="matcha" />
+              <Ctextfield opts="w-1/4" type="email" size="small" variant="outlined" label="email" multiline={false} />
+              <Ctextfield opts="w-1/4" type="password" size="small" variant="outlined" label="password" multiline={false} />
             </div>
-            <div className="text-sm font-thin">Not registered yet ? <a href='#'>Create an account</a></div>
+            <div className="text-sm font-thin pt-6">Not registered yet ? <a href='#'>Create an account</a></div>
           </Paper>
-          <div className='w-1/4 flex-shrink'></div>
+          {/* <div className='w-1/4 flex-shrink'></div> */}
         </div>
-        <Paper className="w-full p-4 border border-white flex flex-col justify-center items-center bg-gray-800">
+        {/* <Paper className="w-full p-4 border border-white flex flex-col justify-center items-center bg-gray-800">
           Copyright
-        </Paper>
-      </div>
+        </Paper> */}
+      {/* </div> */}
     </ThemeProvider>
   );
 }
