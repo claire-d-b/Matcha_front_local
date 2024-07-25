cf497b774ee02c7082a5c7f552b6c67f36fbf46b
claire-d-b <claire.deberny@essca.eu>
2024-07-25 12:10:34 +0200

    Clean code

--- src/app/Cnav.tsx
+++ src/app/Cnav.tsx
@@ -38,63 +38,6 @@ const _: React.FC<ComponentProps> = ({ className }) => {
         <IconButton className="font-thin" size="small"><Logout className="text-base md:text-2xl text-gray-300" /></IconButton>
         <Avatar className="text-sm md:text-2xl md:pt-1">C</Avatar>
       </div>
-      <Menu anchorEl={anchorEl} id="account-menu" open={open} onClose={handleClose} onClick={handleClose}
-        MenuListProps={{
-          sx: {
-            border: '1px solid white',
-            borderRadius: '8px',
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&::before': {
-              content: '""',
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        <MenuItem onClick={handleClose} className="text-sm font-thin">
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem onClick={handleClose} className="text-sm font-thin">
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose} className="text-sm font-thin">
-          <ListItemIcon>
-            <PersonAdd fontSize="small" className="text-sm text-gray-300" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem >
-        <MenuItem onClick={handleClose} className="text-sm font-thin">
-          <ListItemIcon>
-            <Settings fontSize="small" className="text-gray-300" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleClose} className="text-sm font-thin">
-          <ListItemIcon>
-            <Logout fontSize="small" className="text-gray-300" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
     </React.Fragment>
   );
 }
